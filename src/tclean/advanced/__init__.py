9e70c061d7205226d72feb334ea43454c7f54f21
Dano Hawkins <d.d.a.hawkins@tudelft.nl>
2026-08-25 12:08:50 +0200

    Updated public function exposure

--- src/tclean/advanced/__init__.py
+++ src/tclean/advanced/__init__.py
@@ -1,3 +1,8 @@
+"""Advanced time-series cleaning functionality."""
+
+from tclean.advanced.gap_report import build_gap_report
+from tclean.advanced.methods.construct_from_sources import construct_from_sources
+from tclean.advanced.methods.external_profile import read_external_profile
 from tclean.advanced.planning import (
     build_auxiliary_acquisition_requirements,
     build_auxiliary_source_requests,
@@ -7,5 +12,8 @@ from tclean.advanced.planning import (
 __all__ = [
     "build_auxiliary_acquisition_requirements",
     "build_auxiliary_source_requests",
+    "build_gap_report",
+    "construct_from_sources",
+    "read_external_profile",
     "select_active_advanced_rules",
 ]
